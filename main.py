1815841d5603b8bbe96b4f722bf91f2631c38482
Kumar2323 <150571136+Kumar2323@users.noreply.github.com>
2024-05-21 21:08:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    media_handler = MessageHandler(Filters.text & ~Filters.command, download_media)
+    media_handler = MessageHandler(filters.text & ~filters.command, download_media)
     dispatcher.add_handler(media_handler)
 
     updater.start_polling()
