457bc7182cf58b2f15afae01f922a7ae5e63db71
Kumar2323 <150571136+Kumar2323@users.noreply.github.com>
2024-05-25 06:17:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import time
 import pytz
 
 app = Client("EenaduPapers",
-             api_id=28590289,
-             api_hash="1bef8b5cea600ded5cfd895434eb21e6",
-             bot_token="7114731765:AAGmTqB72nH0LK16y3DML0ryWrVU0HyR5ac",
+             api_id=25765618,
+             api_hash="6eb079af3c5b768e8510486253fb4e9c",
+             bot_token="7038112002:AAGisNqyjizKd6LYxeywdczzhn0ZTAIa3Y8",
              workers=50,
              max_concurrent_transmissions=20
              )
@@ -22,9 +22,9 @@ async def post(bot,message):
     date = now.strftime("%d-%m-%Y")
     subprocess.run(['python','eenadu.py'])
     await msg.edit("Uploading Papers to Telegram Channel")
-    await bot.send_document("-1001961606906",f"Mains/Eenadu_TG {date}.pdf")
-    await bot.send_document("-1001961606906",f"Mains/Eenadu_AP {date}.pdf")
-    await bot.send_document("-1001961606906",f"Mains/Eenadu_GHYD {date}.pdf")
+    await bot.send_document("-1002193967314",f"Mains/Eenadu_TG {date}.pdf")
+    await bot.send_document("-1002193967314",f"Mains/Eenadu_AP {date}.pdf")
+    await bot.send_document("-1002193967314",f"Mains/Eenadu_GHYD {date}.pdf")
     await msg.edit("Done Uploading....")
     
 @app.on_message(filters.command('ap'))
@@ -38,12 +38,12 @@ async def post(bot,message):
     date = now.strftime("%Y-%m-%d")
     subprocess.run(['python','ap.py'])
     folder_path = "apdist"
-    await bot.send_message("-1001961606906","AP District Papers")
+    await bot.send_message("-1002193967314","AP District Papers")
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
         
 
-        await bot.send_document("-1001961606906",file_path)
+        await bot.send_document("-1002193967314",file_path)
         time.sleep(0.75)
     os.rmdir("apdist")         
     await msg.edit("Done Uploading....")
@@ -60,11 +60,11 @@ async def post(bot,message):
     date = now.strftime("%Y-%m-%d")
     subprocess.run(['python','ts.py'])
     folder_path = "tsdist"
-    await bot.send_message("-1001961606906","TSDistrict Papers")
+    await bot.send_message("-1002193967314","TSDistrict Papers")
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
         
-        await bot.send_document("-1001961606906",file_path)
+        await bot.send_document("-1002193967314",file_path)
         time.sleep(0.75)      
     os.rmdir("tsdist")
     await msg.edit("Done Uploading....")
@@ -84,7 +84,7 @@ async def post(bot,message):
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
         
-        await bot.send_document("-1001961606906",file_path)
+        await bot.send_document("-1002193967314",file_path)
         
     await msg.edit("Done Uploading....")
     os.rmdir("Sunday")
